2537e09ce3c85e22bd9e3cae5d12f5256a26aa2e
jimmyhex27 <ahmed.gamal@olltek.com>
2024-11-17 10:22:05 +0200

    (fix): performance and accessibility

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <Layout>
-    <router-view/>
+    <HomeView></HomeView>
   </Layout>
 </template>
 
 <script>
 import Layout from "@/layouts/defaultLayout.vue";
+import HomeView from "@/views/HomeView.vue";
 
 export default {
   components: {
+    HomeView,
     Layout,
   },
   data() {
